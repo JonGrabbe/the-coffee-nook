41e4a6476cf73a32910a2151598c4c170e6c15ef
Jon <jongrabbe@outlook.com>
2021-01-20 23:23:45 -0800

    finished mobile menu

---
+++ main.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    document.getElementById('header').classList.remove('mobile-menu-closed');
+    document.getElementById('header').classList.add('mobile-menu-opened');
+}
+
+function closeMenu() {
+    document.getElementById('header').classList.remove('mobile-menu-opened');
+    document.getElementById('header').classList.add('mobile-menu-closed');
+}
